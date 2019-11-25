ea6b66164f3d89b0ea54523cd0f7c64ad9b84454
neep305 <neep305@gmail.com>
2019-11-26 00:25:55 +0900

    add examples for understanding

--- datacamp-marketing-data-analysis/stacked.py
+++ datacamp-marketing-data-analysis/stacked.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def stacked():
-    mul_index = pd.MultiIndex.from_tuples([('cust1','2015'),('cust1','2016'),('cust2','2015'),('cust2','2016')])
+    mul_index = pd.MultiIndex.from_tuples([('cust_1','2015'),('cust_1','2016'),('cust_2','2015'),('cust_2','2016')])
 
     data = pd.DataFrame(data=np.arange(16).reshape(4,4),
                         index=mul_index,
@@ -17,6 +17,47 @@ def stacked():
 
 
 if __name__ == '__main__':
-    data_stack = stacked().stack()
-    print('============<< stacked data >>===========')
-    print(data_stack)
+    data = stacked()
+    data_stacked = stacked().stack()
+    print('\n============<< stacked data >>===========')
+    print(data_stacked)
+
+    # print(data_stack.index)
+
+    print(data_stacked['cust_2']['2015'][['prd_1', 'prd_2']])
+
+    # Nan to DataFrame
+    data.loc['cust_2', 'prd_4'] = np.nan
+    print(data)
+
+    # stack with 'dropna=False'
+    print('\n============<< data having NaN >>===========')
+    print(data.stack(dropna=False))
+
+    # stack with 'dropna=True'
+    print('\n============<< data not having NaN >>===========')
+    print(data.stack(dropna=True))
+
+    print('\n============<< unstack level=-1 >>===========')
+    print(data_stacked.unstack(level=-1))
+
+    print('\n============<< unstack level=0 >>===========')
+    print(data_stacked.unstack(level=0))
+
+    print('\n============<< unstack level=1 >>===========')
+    print(data_stacked.unstack(level=1))
+
+    # converting Series to DataFrame
+    data_stacked_unstacked = data_stacked.unstack(level=-1)
+    print('\n============<< data_stacked.unstack >>===========')
+    print(data_stacked_unstacked)
+
+    # converting index to columns
+    data_stacked_unstacked_df = data_stacked_unstacked.reset_index()
+    print('\n============<< data_stacked_unstacked_df >>===========')
+    print(data_stacked_unstacked_df)
+
+    # changing columns' name
+    print('\n============<< data_stacked_unstacked_df renaming columns >>===========')
+    # data_stacked_unstacked_df = data_stacked_unstacked_df.rename(columns={'level_0': 'customer_id', 'level_1': 'year'}, inplace=True)
+    print(data_stacked_unstacked_df.rename(columns={'prd_1': '상품'}))
